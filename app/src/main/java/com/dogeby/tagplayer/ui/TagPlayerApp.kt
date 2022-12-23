39220787e5b108729b98a5a9640760578b869b26
dogeby <dogeby@gmail.com>
2022-12-24 05:45:57 +0900

    앱 시작 시 권한 허용 여부에 따라 화면 이동 구현
    #5

--- app/src/main/java/com/dogeby/tagplayer/ui/TagPlayerApp.kt
+++ app/src/main/java/com/dogeby/tagplayer/ui/TagPlayerApp.kt
@@ -1,21 +1,40 @@
 package com.dogeby.tagplayer.ui
 
+import android.Manifest
+import android.os.Build
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.dogeby.tagplayer.ui.navigation.HomeRoute
+import com.dogeby.tagplayer.ui.navigation.PermissionRoute
 import com.dogeby.tagplayer.ui.navigation.TagPlayerNavHost
 import com.dogeby.tagplayer.ui.theme.TagPlayerTheme
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.isGranted
+import com.google.accompanist.permissions.rememberPermissionState
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalPermissionsApi::class)
 @Composable
 fun TagPlayerApp() {
+
+    val permissionState = rememberPermissionState(
+        permission = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            Manifest.permission.READ_MEDIA_VIDEO
+        } else {
+            Manifest.permission.READ_EXTERNAL_STORAGE
+        },
+    )
+
     TagPlayerTheme {
         Scaffold { contentPadding ->
             TagPlayerNavHost(
-                modifier = Modifier.padding(contentPadding).fillMaxSize(),
+                modifier = Modifier
+                    .padding(contentPadding)
+                    .fillMaxSize(),
+                startDestination = if (permissionState.status.isGranted) HomeRoute else PermissionRoute,
             )
         }
     }

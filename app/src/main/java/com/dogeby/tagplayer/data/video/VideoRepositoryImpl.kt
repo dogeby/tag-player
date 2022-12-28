ec9eb9144486b8ec6dd49ecc7034115b28a9cccf
dogeby <dogeby@gmail.com>
2022-12-29 04:45:38 +0900

    VideoRepository 캐쉬 기능 추가
    #15

--- app/src/main/java/com/dogeby/tagplayer/data/video/VideoRepositoryImpl.kt
+++ app/src/main/java/com/dogeby/tagplayer/data/video/VideoRepositoryImpl.kt
@@ -1,23 +1,27 @@
 package com.dogeby.tagplayer.data.video
 
 import com.dogeby.tagplayer.data.video.source.local.VideoLocalDataSource
-import com.dogeby.tagplayer.data.video.source.local.toVideo
+import com.dogeby.tagplayer.data.video.source.local.toVideoEntity
+import com.dogeby.tagplayer.database.dao.VideoDao
+import com.dogeby.tagplayer.database.model.toVideo
 import javax.inject.Inject
 import javax.inject.Singleton
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
 @Singleton
 class VideoRepositoryImpl @Inject constructor(
     private val videoRemoteDataSource: VideoLocalDataSource,
+    private val videoDao: VideoDao
 ) : VideoRepository {
 
-    private val _videos = MutableStateFlow<List<Video>>(emptyList())
-    override val videos: StateFlow<List<Video>> = _videos
+    override val videos: Flow<List<Video>> = videoDao.getVideoEntities().map { videoEntities ->
+        videoEntities.map { it.toVideo() }
+    }
 
     override suspend fun updateVideos(): Result<Unit> = runCatching {
         videoRemoteDataSource.getVideoDataList().onSuccess { videoDataList ->
-            _videos.value = videoDataList.map { it.toVideo() }
+            videoDao.cacheVideos(videoDataList.map { it.toVideoEntity() })
         }
     }
 }

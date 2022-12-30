3f8201f06df504c53a98bf923d0af3ca223977cf
dogeby <dogeby@gmail.com>
2022-12-31 01:23:58 +0900

    TagVideoCrossRef foreignKeys 설정
    #16

--- app/src/main/java/com/dogeby/tagplayer/database/model/TagVideoCrossRef.kt
+++ app/src/main/java/com/dogeby/tagplayer/database/model/TagVideoCrossRef.kt
@@ -2,10 +2,25 @@ package com.dogeby.tagplayer.database.model
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.ForeignKey
 
 @Entity(
     tableName = "tag_video_cross_refs",
     primaryKeys = ["tag_id", "video_id"],
+    foreignKeys = [
+        ForeignKey(
+            entity = TagEntity::class,
+            parentColumns = ["id"],
+            childColumns = ["tag_id"],
+            onDelete = ForeignKey.CASCADE,
+        ),
+        ForeignKey(
+            entity = VideoEntity::class,
+            parentColumns = ["id"],
+            childColumns = ["video_id"],
+            onDelete = ForeignKey.CASCADE,
+        ),
+    ],
 )
 data class TagVideoCrossRef(
     @ColumnInfo(name = "tag_id") val tagId: Long,

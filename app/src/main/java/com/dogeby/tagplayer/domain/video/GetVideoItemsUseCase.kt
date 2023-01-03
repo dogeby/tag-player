468e316113266a9df56ba77a60cb9503718d92ad
dogeby <dogeby@gmail.com>
2023-01-04 07:45:03 +0900

    GetVideoItemsUseCase 태그 필터링에 따라 반환하게 수정
    #9

--- app/src/main/java/com/dogeby/tagplayer/domain/video/GetVideoItemsUseCase.kt
+++ app/src/main/java/com/dogeby/tagplayer/domain/video/GetVideoItemsUseCase.kt
@@ -1,20 +1,32 @@
 package com.dogeby.tagplayer.domain.video
 
+import com.dogeby.tagplayer.data.preferences.PreferencesRepository
 import com.dogeby.tagplayer.data.video.VideoRepository
 import com.dogeby.tagplayer.data.video.VideoWithTags
 import javax.inject.Inject
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.map
 
 class GetVideoItemsUseCase @Inject constructor(
     private val videoRepository: VideoRepository,
+    private val preferencesRepository: PreferencesRepository,
     private val formatDurationUseCase: FormatDurationUseCase,
 ) {
 
-    operator fun invoke(tagIdsToFilter: List<Long>): Flow<List<VideoItem>> {
-        val videosWithTags = if (tagIdsToFilter.isEmpty()) videoRepository.videosWithTags
-        else videoRepository.getVideosWithTagsFilteredByTag(tagIdsToFilter)
-        return videosWithTags.mapToVideoItems()
+    @OptIn(ExperimentalCoroutinesApi::class)
+    operator fun invoke(): Flow<List<VideoItem>> {
+        return preferencesRepository.videoListPreferencesData.flatMapLatest { videoListPreferencesData ->
+            val filteredTagIds = videoListPreferencesData.filteredTagIds
+            val videosWithTags = if (filteredTagIds.isEmpty()) {
+                videoRepository.videosWithTags
+            } else {
+                videoRepository.getVideosWithTagsFilteredByTag(filteredTagIds)
+            }
+
+            videosWithTags.mapToVideoItems()
+        }
     }
 
     private fun Flow<List<VideoWithTags>>.mapToVideoItems(): Flow<List<VideoItem>> =

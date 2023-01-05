ab644ac9ad48c216928396241d7bbd131f241a6e
dogeby <dogeby@gmail.com>
2023-01-06 07:26:27 +0900

    동영상 리스트 설정 chip 모음 composable 구현
    #19

--- app/src/main/java/com/dogeby/tagplayer/ui/videolist/VideoListScreen.kt
+++ app/src/main/java/com/dogeby/tagplayer/ui/videolist/VideoListScreen.kt
@@ -1,5 +1,6 @@
 package com.dogeby.tagplayer.ui.videolist
 
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.material3.AssistChip
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -9,10 +10,36 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import com.dogeby.tagplayer.R
 import com.dogeby.tagplayer.ui.theme.TagPlayerTheme
+
+@Composable
+fun VideoListSetting(
+    filteredTagsUiState: FilteredTagsUiState,
+    onNavigateToFilterSetting: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    val filteredTags = if (filteredTagsUiState is FilteredTagsUiState.Success) {
+        filteredTagsUiState.filteredTagNames.joinToString(prefix = ": ")
+    } else {
+        ""
+    }
+    Row(modifier = modifier) {
+        SettingAssistChip(
+            text = "${stringResource(id = R.string.videolist_filter)}$filteredTags",
+            onClick = onNavigateToFilterSetting,
+            leadingIcon = {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_filter_list),
+                    contentDescription = null,
+                )
+            }
+        )
+    }
+}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -28,6 +55,17 @@ fun SettingAssistChip(
         modifier = modifier.widthIn(max = dimensionResource(id = R.dimen.videolist_setting_assist_chip_max_width)),
         leadingIcon = leadingIcon,
     )
+}
+
+@Preview(showBackground = true, heightDp = 640, widthDp = 360)
+@Composable
+fun VideoListSettingPreview() {
+    TagPlayerTheme {
+        VideoListSetting(
+            filteredTagsUiState = FilteredTagsUiState.Success(List(4) { "tag$it" }),
+            onNavigateToFilterSetting = { },
+        )
+    }
 }
 
 @Preview(showBackground = true)

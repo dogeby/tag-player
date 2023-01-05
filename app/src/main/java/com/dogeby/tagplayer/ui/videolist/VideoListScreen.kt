03cba0bdb0fe426cd3d48d5bd786a16fc46ebe5b
dogeby <dogeby@gmail.com>
2023-01-06 06:47:57 +0900

    SettingAssistChip text 한 줄, width 제한 및 ellipsis하게 수정
    #19

--- app/src/main/java/com/dogeby/tagplayer/ui/videolist/VideoListScreen.kt
+++ app/src/main/java/com/dogeby/tagplayer/ui/videolist/VideoListScreen.kt
@@ -1,12 +1,15 @@
 package com.dogeby.tagplayer.ui.videolist
 
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.material3.AssistChip
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import com.dogeby.tagplayer.R
 import com.dogeby.tagplayer.ui.theme.TagPlayerTheme
@@ -21,8 +24,8 @@ fun SettingAssistChip(
 ) {
     AssistChip(
         onClick = onClick,
-        label = { Text(text = text) },
-        modifier = modifier,
+        label = { Text(text = text, maxLines = 1, overflow = TextOverflow.Ellipsis) },
+        modifier = modifier.widthIn(max = dimensionResource(id = R.dimen.videolist_setting_assist_chip_max_width)),
         leadingIcon = leadingIcon,
     )
 }

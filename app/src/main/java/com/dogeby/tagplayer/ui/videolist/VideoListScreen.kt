063fdc5bdc72f9f940a3bd3689743e0ad02ea5fe
dogeby <dogeby@gmail.com>
2023-01-10 15:16:25 +0900

    VideoListScreen, VideoList, VideoItem 구현
    #19

--- app/src/main/java/com/dogeby/tagplayer/ui/videolist/VideoListScreen.kt
+++ app/src/main/java/com/dogeby/tagplayer/ui/videolist/VideoListScreen.kt
@@ -1,29 +1,100 @@
 package com.dogeby.tagplayer.ui.videolist
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.AssistChip
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.integerResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
 import com.bumptech.glide.integration.compose.GlideImage
 import com.dogeby.tagplayer.R
+import com.dogeby.tagplayer.domain.video.VideoItem
 import com.dogeby.tagplayer.ui.theme.TagPlayerTheme
+
+@Composable
+fun VideoListRoute(
+    onNavigateToPlayer: () -> Unit,
+    onNavigateToFilterSetting: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: VideoListViewModel = hiltViewModel(),
+) {
+    val videoListUiState: VideoListUiState by viewModel.videoListUiState.collectAsState()
+    val filteredTagsUiState: FilteredTagsUiState by viewModel.filteredTagsUiState.collectAsState()
+
+    VideoListScreen(
+        videoListUiState = videoListUiState,
+        filteredTagsUiState = filteredTagsUiState,
+        onNavigateToPlayer,
+        modifier = modifier.fillMaxWidth(),
+        onNavigateToFilterSetting = onNavigateToFilterSetting,
+    )
+}
+
+@Composable
+fun VideoListScreen(
+    videoListUiState: VideoListUiState,
+    filteredTagsUiState: FilteredTagsUiState,
+    onNavigateToPlayer: () -> Unit,
+    onNavigateToFilterSetting: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    var progressIndicatorState by rememberSaveable { mutableStateOf(videoListUiState is VideoListUiState.Loading) }
+    if (progressIndicatorState) LinearProgressIndicator(modifier = modifier)
+
+    Column(
+        modifier = modifier.padding(horizontal = dimensionResource(id = R.dimen.padding_small)),
+    ) {
+        VideoListSetting(
+            filteredTagsUiState = filteredTagsUiState,
+            onNavigateToFilterSetting = onNavigateToFilterSetting,
+        )
+
+        when (videoListUiState) {
+            VideoListUiState.Loading -> {
+                progressIndicatorState = true
+            }
+            is VideoListUiState.Success -> {
+                progressIndicatorState = false
+                VideoList(
+                    videoListUiState = videoListUiState,
+                    onNavigateToPlayer = onNavigateToPlayer,
+                )
+            }
+        }
+    }
+}
 
 @Composable
 fun VideoListSetting(
@@ -51,6 +122,25 @@ fun VideoListSetting(
     }
 }
 
+@Composable
+fun VideoList(
+    videoListUiState: VideoListUiState.Success,
+    onNavigateToPlayer: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    LazyColumn(
+        modifier = modifier,
+    ) {
+        items(videoListUiState.videoItems) { videoItem ->
+            VideoItem(
+                videoItem = videoItem,
+                onNavigateToPlayer = onNavigateToPlayer,
+                modifier = Modifier.padding(vertical = dimensionResource(id = R.dimen.padding_small)),
+            )
+        }
+    }
+}
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SettingAssistChip(
@@ -65,6 +155,53 @@ fun SettingAssistChip(
         modifier = modifier.widthIn(max = dimensionResource(id = R.dimen.videolist_setting_assist_chip_max_width)),
         leadingIcon = leadingIcon,
     )
+}
+
+@Composable
+fun VideoItem(
+    videoItem: VideoItem,
+    onNavigateToPlayer: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(dimensionResource(id = R.dimen.videolist_video_item_height))
+            .clickable { onNavigateToPlayer() },
+    ) {
+        VideoThumbnail(
+            uri = videoItem.uri,
+            width = integerResource(id = R.integer.videolist_video_thumbnail_width),
+            height = integerResource(id = R.integer.videolist_video_thumbnail_height),
+            duration = videoItem.duration,
+            modifier = Modifier.width(dimensionResource(id = R.dimen.videolist_video_thumbnail_width)),
+        )
+        Box(
+            modifier = Modifier
+                .fillMaxHeight()
+                .padding(dimensionResource(id = R.dimen.videolist_video_item_info_padding)),
+        ) {
+            Text(text = videoItem.name, maxLines = 2, overflow = TextOverflow.Ellipsis)
+
+            val tagListItemHorizontalPadding = Modifier.padding(horizontal = dimensionResource(id = R.dimen.videolist_video_tag_list_item_horizontal_padding))
+            LazyRow(modifier = Modifier.align(Alignment.BottomStart)) {
+                items(videoItem.parentDirectories) {
+                    VideoTag(
+                        name = it,
+                        color = MaterialTheme.colorScheme.secondary,
+                        modifier = tagListItemHorizontalPadding,
+                    )
+                }
+                items(videoItem.tags) {
+                    VideoTag(
+                        name = it.name,
+                        color = MaterialTheme.colorScheme.tertiary,
+                        modifier = tagListItemHorizontalPadding,
+                    )
+                }
+            }
+        }
+    }
 }
 
 @OptIn(ExperimentalGlideComposeApi::class)

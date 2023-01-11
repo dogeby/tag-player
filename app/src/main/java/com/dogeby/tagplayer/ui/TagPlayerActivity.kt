a508288dbcb02754d67351ed72e7cdd768b95623
dogeby <dogeby@gmail.com>
2023-01-12 04:21:56 +0900

    TagPlayerApp에서 권한 허용 여부를 가져오던것을 activity 위치로 수정하기
    #27

--- app/src/main/java/com/dogeby/tagplayer/ui/TagPlayerActivity.kt
+++ app/src/main/java/com/dogeby/tagplayer/ui/TagPlayerActivity.kt
@@ -1,5 +1,8 @@
 package com.dogeby.tagplayer.ui
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -9,12 +12,21 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class TagPlayerActivity : ComponentActivity() {
 
+    private val isRequiredPermissionsGranted: Boolean
+        get() = checkSelfPermission(
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                Manifest.permission.READ_MEDIA_VIDEO
+            } else {
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            },
+        ) == PackageManager.PERMISSION_GRANTED
+
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
         super.onCreate(savedInstanceState)
 
         setContent {
-            TagPlayerApp()
+            TagPlayerApp(isRequiredPermissionsGranted)
         }
     }
 }

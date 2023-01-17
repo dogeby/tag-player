771251b4fc04baab506f4dd8a5b8575e7e21b4a2
dogeby <dogeby@gmail.com>
2023-01-17 12:57:21 +0900

    VideoListScreen SettingAssistChip 삭제
    #42

--- app/src/main/java/com/dogeby/tagplayer/ui/videolist/VideoListScreen.kt
+++ app/src/main/java/com/dogeby/tagplayer/ui/videolist/VideoListScreen.kt
@@ -1,12 +1,10 @@
 package com.dogeby.tagplayer.ui.videolist
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Icon
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -17,14 +15,10 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.dogeby.tagplayer.R
 import com.dogeby.tagplayer.ui.permission.AppPermissionDeniedByExternalAction
 import com.dogeby.tagplayer.ui.permission.AppRequiredPermission
-import com.dogeby.tagplayer.ui.theme.TagPlayerTheme
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.PermissionState
 import com.google.accompanist.permissions.isGranted
@@ -40,7 +34,6 @@ fun VideoListRoute(
     viewModel: VideoListViewModel = hiltViewModel(),
 ) {
     val videoListUiState: VideoListUiState by viewModel.videoListUiState.collectAsState()
-    val filteredTagsUiState: FilteredTagsUiState by viewModel.filteredTagsUiState.collectAsState()
     val isSelectMode: Boolean by viewModel.isSelectMode.collectAsState()
     val isSelectedVideoItems: Map<Long, Boolean> = viewModel.isSelectedVideoItems
 
@@ -54,7 +47,6 @@ fun VideoListRoute(
 
     VideoListScreen(
         videoListUiState = videoListUiState,
-        filteredTagsUiState = filteredTagsUiState,
         isSelectMode = isSelectMode,
         isSelectedVideoItems = isSelectedVideoItems.toMap(),
         onNavigateToPlayer = onNavigateToPlayer,
@@ -67,7 +59,6 @@ fun VideoListRoute(
 @Composable
 fun VideoListScreen(
     videoListUiState: VideoListUiState,
-    filteredTagsUiState: FilteredTagsUiState,
     isSelectMode: Boolean,
     isSelectedVideoItems: Map<Long, Boolean>,
     onNavigateToPlayer: () -> Unit,
@@ -79,13 +70,8 @@ fun VideoListScreen(
     if (progressIndicatorState) LinearProgressIndicator(modifier = modifier)
 
     Column(
-        modifier = modifier.padding(horizontal = dimensionResource(id = R.dimen.padding_small)),
+        modifier = modifier.padding(dimensionResource(id = R.dimen.padding_small)),
     ) {
-        VideoListSetting(
-            filteredTagsUiState = filteredTagsUiState,
-            onNavigateToFilterSetting = onNavigateToFilterSetting,
-        )
-
         when (videoListUiState) {
             VideoListUiState.Loading -> {
                 progressIndicatorState = true
@@ -101,32 +87,6 @@ fun VideoListScreen(
                 )
             }
         }
-    }
-}
-
-@Composable
-fun VideoListSetting(
-    filteredTagsUiState: FilteredTagsUiState,
-    onNavigateToFilterSetting: () -> Unit,
-    modifier: Modifier = Modifier,
-) {
-    val filteredTags =
-        if (filteredTagsUiState is FilteredTagsUiState.Success && filteredTagsUiState.filteredTagNames.isNotEmpty()) {
-            filteredTagsUiState.filteredTagNames.joinToString(prefix = ": ")
-        } else {
-            ""
-        }
-    Row(modifier = modifier) {
-        SettingAssistChip(
-            text = "${stringResource(id = R.string.videolist_filter)}$filteredTags",
-            onClick = onNavigateToFilterSetting,
-            leadingIcon = {
-                Icon(
-                    painter = painterResource(id = R.drawable.ic_filter_list),
-                    contentDescription = null,
-                )
-            },
-        )
     }
 }
 
@@ -157,16 +117,5 @@ fun VideoList(
                 modifier = Modifier.padding(bottom = dimensionResource(id = R.dimen.padding_small)),
             )
         }
-    }
-}
-
-@Preview(showBackground = true, heightDp = 640, widthDp = 360)
-@Composable
-fun VideoListSettingPreview() {
-    TagPlayerTheme {
-        VideoListSetting(
-            filteredTagsUiState = FilteredTagsUiState.Success(List(4) { "tag$it" }),
-            onNavigateToFilterSetting = { },
-        )
     }
 }
--- app/src/main/java/com/dogeby/tagplayer/ui/videolist/VideoListViewModel.kt
+++ app/src/main/java/com/dogeby/tagplayer/ui/videolist/VideoListViewModel.kt
@@ -3,8 +3,6 @@ package com.dogeby.tagplayer.ui.videolist
 import androidx.compose.runtime.mutableStateMapOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.dogeby.tagplayer.data.tag.Tag
-import com.dogeby.tagplayer.domain.preferences.GetFilteredTagUseCase
 import com.dogeby.tagplayer.domain.video.GetVideoItemsUseCase
 import com.dogeby.tagplayer.domain.video.UpdateVideoListUseCase
 import com.dogeby.tagplayer.domain.video.VideoItem
@@ -19,7 +17,6 @@ import kotlinx.coroutines.flow.stateIn
 
 @HiltViewModel
 class VideoListViewModel @Inject constructor(
-    getFilteredTagUseCase: GetFilteredTagUseCase,
     getVideoItemsUseCase: GetVideoItemsUseCase,
     private val updateVideoListUseCase: UpdateVideoListUseCase,
 ) : ViewModel() {
@@ -29,15 +26,6 @@ class VideoListViewModel @Inject constructor(
 
     private val _isSelectedVideoItems = mutableStateMapOf<Long, Boolean>()
     val isSelectedVideoItems: Map<Long, Boolean> = _isSelectedVideoItems
-
-    val filteredTagsUiState: StateFlow<FilteredTagsUiState> = getFilteredTagUseCase()
-        .map<List<Tag>, FilteredTagsUiState> { tags -> FilteredTagsUiState.Success(tags.map { it.name }) }
-        .onStart { emit(FilteredTagsUiState.Loading) }
-        .stateIn(
-            scope = viewModelScope,
-            started = SharingStarted.WhileSubscribed(5_000),
-            initialValue = FilteredTagsUiState.Loading,
-        )
 
     val videoListUiState: StateFlow<VideoListUiState> = getVideoItemsUseCase()
         .map<List<VideoItem>, VideoListUiState>(VideoListUiState::Success)

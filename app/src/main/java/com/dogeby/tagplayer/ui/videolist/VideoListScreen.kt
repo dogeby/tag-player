2203b0aad3b5cb5357001f21573f0ff313cb129a
dogeby <dogeby@gmail.com>
2023-01-18 21:53:02 +0900

    밑으로 리스트 드래그 시 BottomAppBar hide 구현
    버그 #43
    
    #42

--- app/src/main/java/com/dogeby/tagplayer/ui/videolist/VideoListScreen.kt
+++ app/src/main/java/com/dogeby/tagplayer/ui/videolist/VideoListScreen.kt
@@ -1,24 +1,36 @@
 package com.dogeby.tagplayer.ui.videolist
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.shrinkVertically
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.dogeby.tagplayer.R
@@ -78,51 +90,68 @@ fun VideoListScreen(
     var progressIndicatorState by rememberSaveable { mutableStateOf(videoListUiState is VideoListUiState.Loading) }
     if (progressIndicatorState) LinearProgressIndicator(modifier = modifier)
 
-    var currentIsSelectMode by remember { mutableStateOf(isSelectMode) }
-    val isShowBottomAppBarIconAnimation by rememberUpdatedState(
-        newValue = currentIsSelectMode != isSelectMode
-    )
-    currentIsSelectMode = isSelectMode
+    var isShowBottomAppBarIconAnimation by remember { mutableStateOf(false) }
+
+    var bottomBarShown by rememberSaveable { mutableStateOf(true) }
 
     Scaffold(
         modifier = modifier,
         bottomBar = {
-            if (isSelectMode) {
-                VideoItemBottomAppBar(
-                    onAllItemSelectButtonClick = { /*TODO*/ },
-                    onTagSettingButtonClick = { /*TODO*/ },
-                    onInfoButtonClick = { /*TODO*/ },
-                    isShowAnimation = isShowBottomAppBarIconAnimation
-                )
-            } else {
-                VideoListBottomAppBar(
-                    isFilterButtonChecked = isFilteredTag,
-                    onSearchButtonClick = { /*TODO*/ },
-                    onFilterButtonClick = onNavigateToFilterSetting,
-                    onSortButtonClick = { /*TODO*/ },
-                    isShowAnimation = isShowBottomAppBarIconAnimation
-                )
-            }
-        },
-    ) { contentPadding ->
-        Column(
-            modifier = Modifier.padding(contentPadding),
-        ) {
-            when (videoListUiState) {
-                VideoListUiState.Loading -> {
-                    progressIndicatorState = true
-                }
-                is VideoListUiState.Success -> {
-                    progressIndicatorState = false
-                    VideoList(
-                        videoListUiState = videoListUiState,
-                        isSelectMode = isSelectMode,
-                        isSelectedVideoItems = isSelectedVideoItems,
-                        onNavigateToPlayer = onNavigateToPlayer,
-                        onToggleVideoItem = onToggleVideoItem,
-                        modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_small)),
+            AnimatedVisibility(
+                modifier = Modifier.background(Color.Transparent),
+                visible = bottomBarShown,
+                enter = slideInVertically(
+                    animationSpec = tween(
+                        durationMillis = 250,
+                    ),
+                    initialOffsetY = { it },
+                ) +
+                    expandVertically() +
+                    fadeIn(initialAlpha = 0.3f),
+                exit = slideOutVertically(
+                    animationSpec = tween(
+                        durationMillis = 200,
+                    ),
+                    targetOffsetY = { it },
+                ) +
+                    shrinkVertically() +
+                    fadeOut(),
+            ) {
+                if (isSelectMode) {
+                    VideoItemBottomAppBar(
+                        onAllItemSelectButtonClick = { /*TODO*/ },
+                        onTagSettingButtonClick = { /*TODO*/ },
+                        onInfoButtonClick = { /*TODO*/ },
+                        isShowAnimation = isShowBottomAppBarIconAnimation
+                    )
+                } else {
+                    VideoListBottomAppBar(
+                        isFilterButtonChecked = isFilteredTag,
+                        onSearchButtonClick = { /*TODO*/ },
+                        onFilterButtonClick = onNavigateToFilterSetting,
+                        onSortButtonClick = { /*TODO*/ },
+                        isShowAnimation = isShowBottomAppBarIconAnimation
                     )
                 }
+            }
+            isShowBottomAppBarIconAnimation = true
+        },
+    ) { contentPadding ->
+        when (videoListUiState) {
+            VideoListUiState.Loading -> {
+                progressIndicatorState = true
+            }
+            is VideoListUiState.Success -> {
+                progressIndicatorState = false
+                VideoList(
+                    modifier = Modifier.padding(contentPadding),
+                    videoListUiState = videoListUiState,
+                    isSelectMode = isSelectMode,
+                    isSelectedVideoItems = isSelectedVideoItems,
+                    onNavigateToPlayer = onNavigateToPlayer,
+                    onToggleVideoItem = onToggleVideoItem,
+                    onScrollingUp = { bottomBarShown = isSelectMode || it },
+                )
             }
         }
     }
@@ -135,10 +164,16 @@ fun VideoList(
     isSelectMode: Boolean,
     onNavigateToPlayer: () -> Unit,
     onToggleVideoItem: (Long) -> Unit,
+    onScrollingUp: (Boolean) -> Unit,
     modifier: Modifier = Modifier,
 ) {
+    val videoListState = rememberLazyListState()
+    onScrollingUp(videoListState.isScrollingUp())
+
     LazyColumn(
         modifier = modifier,
+        state = videoListState,
+        contentPadding = PaddingValues(dimensionResource(id = R.dimen.padding_small)),
         verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_small)),
     ) {
         items(videoListUiState.videoItems) { videoItem ->
@@ -156,4 +191,22 @@ fun VideoList(
             )
         }
     }
+}
+
+@Composable
+private fun LazyListState.isScrollingUp(): Boolean {
+    var previousIndex by remember(this) { mutableStateOf(firstVisibleItemIndex) }
+    var previousScrollOffset by remember(this) { mutableStateOf(firstVisibleItemScrollOffset) }
+    return remember(this) {
+        derivedStateOf {
+            if (previousIndex != firstVisibleItemIndex) {
+                previousIndex > firstVisibleItemIndex
+            } else {
+                previousScrollOffset >= firstVisibleItemScrollOffset
+            }.also {
+                previousIndex = firstVisibleItemIndex
+                previousScrollOffset = firstVisibleItemScrollOffset
+            }
+        }
+    }.value
 }

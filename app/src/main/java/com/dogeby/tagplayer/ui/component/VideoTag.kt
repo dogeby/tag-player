93ea216556f9899d22a528334636e25988980ab1
dogeby <dogeby@gmail.com>
2023-01-31 14:43:54 +0900

    VideoTag shape, textStyle 입력 받을수 있게 수정
    #51

--- app/src/main/java/com/dogeby/tagplayer/ui/component/VideoTag.kt
+++ app/src/main/java/com/dogeby/tagplayer/ui/component/VideoTag.kt
@@ -7,7 +7,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.text.TextStyle
 import com.dogeby.tagplayer.R
 
 @Composable
@@ -15,10 +17,12 @@ fun VideoTag(
     name: String,
     color: Color,
     modifier: Modifier = Modifier,
+    shape: Shape = MaterialTheme.shapes.small,
+    textStyle: TextStyle = MaterialTheme.typography.labelMedium,
 ) {
     Surface(
         modifier = modifier,
-        shape = MaterialTheme.shapes.small,
+        shape = shape,
         color = color,
     ) {
         Text(
@@ -26,7 +30,7 @@ fun VideoTag(
             modifier = Modifier
                 .padding(horizontal = dimensionResource(id = R.dimen.videolist_video_tag_horizontal_padding)),
             maxLines = 1,
-            style = MaterialTheme.typography.labelMedium,
+            style = textStyle,
         )
     }
 }

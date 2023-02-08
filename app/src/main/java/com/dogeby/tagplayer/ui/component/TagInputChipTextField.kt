207b94a87fd3e0159e3a592ce551ae8bb2a172c0
dogeby <dogeby@gmail.com>
2023-02-09 06:58:09 +0900

    키보드 바깥 빈 공간을 탭 시 키보드가 내려가게 수정
    #60

--- app/src/main/java/com/dogeby/tagplayer/ui/component/TagInputChipTextField.kt
+++ app/src/main/java/com/dogeby/tagplayer/ui/component/TagInputChipTextField.kt
@@ -1,6 +1,7 @@
 package com.dogeby.tagplayer.ui.component
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
@@ -26,9 +27,12 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
@@ -130,6 +134,16 @@ fun InputChipTextField(
                     }
                 },
             )
+        }
+    }
+}
+
+fun Modifier.clearFocusWhenTap(doOnClear: () -> Unit = {}): Modifier = composed {
+    val focusManager = LocalFocusManager.current
+    this.pointerInput(Unit) {
+        detectTapGestures {
+            focusManager.clearFocus()
+            doOnClear()
         }
     }
 }

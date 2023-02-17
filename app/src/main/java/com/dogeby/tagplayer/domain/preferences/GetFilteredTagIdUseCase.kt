abdac2bdaa360b66a3554e74b161f9372348fd14
dogeby <dogeby@gmail.com>
2023-02-17 19:36:01 +0900

    필터링 하는 태그 ID를 가져오는 UseCase 구현
    #69

---
+++ app/src/main/java/com/dogeby/tagplayer/domain/preferences/GetFilteredTagIdUseCase.kt
@@ -0,0 +1,17 @@
+package com.dogeby.tagplayer.domain.preferences
+
+import com.dogeby.tagplayer.data.preferences.PreferencesRepository
+import javax.inject.Inject
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+class GetFilteredTagIdUseCase @Inject constructor(
+    private val preferencesRepository: PreferencesRepository,
+) {
+
+    operator fun invoke(): Flow<List<Long>> {
+        return preferencesRepository.videoListPreferencesData.map {
+            it.filteredTagIds
+        }
+    }
+}

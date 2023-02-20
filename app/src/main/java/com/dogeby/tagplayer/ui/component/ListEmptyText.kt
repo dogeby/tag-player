3115b914eb59f47acbabcee94a3200f6c28261ef
dogeby <dogeby@gmail.com>
2023-02-21 00:43:32 +0900

    목록이 비었다는 문구를 표시하는 Composable 구현
    #80

---
+++ app/src/main/java/com/dogeby/tagplayer/ui/component/ListEmptyText.kt
@@ -0,0 +1,23 @@
+package com.dogeby.tagplayer.ui.component
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ListEmptyText(
+    text: String,
+    modifier: Modifier = Modifier,
+) {
+    Box(
+        modifier = modifier.fillMaxSize()
+    ) {
+        Text(
+            text = text,
+            modifier = Modifier.align(Alignment.Center),
+        )
+    }
+}

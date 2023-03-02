7d186e8f58635ee3c4f34c1fffe4256573efe79a
dogeby <dogeby@gmail.com>
2023-03-02 15:35:53 +0900

    스와이프로 동영상 탐색할수있는 VideoPlayerPager 구현
    #92

---
+++ app/src/main/java/com/dogeby/tagplayer/ui/videoplayer/VideoPlayerPager.kt
@@ -0,0 +1,49 @@
+package com.dogeby.tagplayer.ui.videoplayer
+
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.pager.VerticalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.media3.common.util.UnstableApi
+import com.dogeby.tagplayer.domain.video.VideoItem
+
+@OptIn(ExperimentalFoundationApi::class)
+@androidx.annotation.OptIn(UnstableApi::class)
+@Composable
+fun VideoPlayerPager(
+    currentPageVideoId: Long,
+    videoItems: List<VideoItem>,
+    onSettledPageChanged: (videoId: Long) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    val pagerState = rememberPagerState(
+        videoItems
+            .indexOfFirst { it.id == currentPageVideoId }
+            .coerceAtLeast(0),
+    )
+    val settledPageVideoId by remember {
+        derivedStateOf {
+            videoItems[pagerState.settledPage].id
+        }
+    }
+    onSettledPageChanged(settledPageVideoId)
+
+    VerticalPager(
+        pageCount = videoItems.size,
+        state = pagerState,
+        modifier = modifier,
+        key = { videoItems[it].id }
+    ) { index ->
+        val videoPlayerItem = videoItems[index]
+        VideoPlayer(
+            videoItem = videoPlayerItem,
+            playWhenReady = videoPlayerItem.id == settledPageVideoId,
+            modifier = Modifier.fillMaxSize()
+        )
+    }
+}

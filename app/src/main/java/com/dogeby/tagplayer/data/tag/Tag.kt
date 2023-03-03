bfbe5e20554facb38b77bd0edf9d83480c8b9002
dogeby <dogeby@gmail.com>
2023-03-03 12:51:38 +0900

    Tag에 Comparable 추가
    #97

--- app/src/main/java/com/dogeby/tagplayer/data/tag/Tag.kt
+++ app/src/main/java/com/dogeby/tagplayer/data/tag/Tag.kt
@@ -5,7 +5,12 @@ import com.dogeby.tagplayer.database.model.TagEntity
 data class Tag(
     val id: Long = 0,
     val name: String,
-)
+) : Comparable<Tag> {
+
+    override fun compareTo(other: Tag): Int {
+        return id.compareTo(other.id)
+    }
+}
 
 fun Tag.toTagEntity() =
     TagEntity(

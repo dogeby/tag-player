0a86d95979fed8d85bd511c638b85de26ebf8c6c
dogeby <dogeby@gmail.com>
2023-03-12 19:52:30 +0900

    동영상 업데이트 시 폴더 필터 체크 후 삭제된 폴더면 필터에서 삭제
    #91

--- app/src/main/java/com/dogeby/tagplayer/domain/video/UpdateVideoListUseCase.kt
+++ app/src/main/java/com/dogeby/tagplayer/domain/video/UpdateVideoListUseCase.kt
@@ -1,13 +1,23 @@
 package com.dogeby.tagplayer.domain.video
 
 import com.dogeby.tagplayer.data.video.VideoRepository
+import com.dogeby.tagplayer.domain.preferences.GetFilteredDirectoryNameUseCase
+import com.dogeby.tagplayer.domain.preferences.RemoveDirectoryFilterUseCase
 import javax.inject.Inject
+import kotlinx.coroutines.flow.first
 
 class UpdateVideoListUseCase @Inject constructor(
-    private val videoRepository: VideoRepository
+    private val videoRepository: VideoRepository,
+    private val getAllDirectoriesUseCase: GetAllDirectoriesUseCase,
+    private val getFilteredDirectoryNameUseCase: GetFilteredDirectoryNameUseCase,
+    private val removeDirectoryFilterUseCase: RemoveDirectoryFilterUseCase,
 ) {
 
     suspend operator fun invoke(): Result<Unit> {
-        return videoRepository.updateVideos()
+        return videoRepository.updateVideos().onSuccess {
+            val allDirectories = getAllDirectoriesUseCase().first()
+            val filteredDirectoryNames = getFilteredDirectoryNameUseCase().first()
+            removeDirectoryFilterUseCase(filteredDirectoryNames - allDirectories)
+        }
     }
 }

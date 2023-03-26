388c293214f885f34e105df8259defec5b159a9b
dogeby <dogeby@gmail.com>
2023-03-27 07:25:20 +0900

    VideoPlayerScreen Loading 상태 시 로딩 원 추가
    #127

--- app/src/main/java/com/dogeby/tagplayer/ui/videoplayer/VideoPlayerScreen.kt
+++ app/src/main/java/com/dogeby/tagplayer/ui/videoplayer/VideoPlayerScreen.kt
@@ -4,13 +4,16 @@ import android.annotation.SuppressLint
 import android.content.pm.ActivityInfo
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
@@ -20,13 +23,17 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.view.WindowInsetsControllerCompat
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.dogeby.tagplayer.ui.findActivity
+import com.dogeby.tagplayer.ui.theme.PlayerBackgroundColor
 import com.dogeby.tagplayer.ui.theme.PlayerControllerBackgroundColor
 import com.dogeby.tagplayer.ui.theme.PlayerControllerOnBackgroundColor
+import com.dogeby.tagplayer.ui.theme.TagPlayerTheme
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 @Composable
@@ -98,7 +105,16 @@ fun VideoPlayerScreen(
         },
     ) {
         when (videoPlayerPagerUiState) {
-            VideoPlayerPagerUiState.Loading -> { /*TODO*/ }
+            VideoPlayerPagerUiState.Loading -> {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = PlayerBackgroundColor,
+                ) {
+                    Box(contentAlignment = Alignment.Center) {
+                        CircularProgressIndicator()
+                    }
+                }
+            }
 
             VideoPlayerPagerUiState.Empty -> { /*TODO*/ }
 
@@ -119,5 +135,17 @@ fun VideoPlayerScreen(
                 )
             }
         }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun VideoPlayerScreenLoadingPreview() {
+    TagPlayerTheme {
+        VideoPlayerScreen(
+            videoPlayerPagerUiState = VideoPlayerPagerUiState.Loading,
+            onPlayerSettledPageChanged = {},
+            onNavigateUp = {},
+        )
     }
 }

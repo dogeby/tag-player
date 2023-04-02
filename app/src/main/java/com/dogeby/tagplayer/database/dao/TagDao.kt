5cc84f9cc694efab909e8389f05868e94b39a5b1
dogeby <dogeby@gmail.com>
2023-04-02 14:44:47 +0900

    TagDao insertTags onConflict 시 IGNORE이 아닌 ABORT로 수정
    중복 이름으로 태그 생성 시 Result가 Success로 나오는 문제 해결
    
    #135

--- app/src/main/java/com/dogeby/tagplayer/database/dao/TagDao.kt
+++ app/src/main/java/com/dogeby/tagplayer/database/dao/TagDao.kt
@@ -11,7 +11,7 @@ import kotlinx.coroutines.flow.Flow
 @Dao
 interface TagDao {
 
-    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    @Insert(onConflict = OnConflictStrategy.ABORT)
     suspend fun insertTags(entities: List<TagEntity>): List<Long>
 
     @Update(onConflict = OnConflictStrategy.IGNORE)

d07aa5c0267ba4f41d1fe7b6e0d758e9f6aefe29
dogeby <dogeby@gmail.com>
2023-04-14 00:16:53 +0900

    앱 환경 설정 proto datastore 구현
    #166

---
+++ app/src/main/java/com/dogeby/tagplayer/datastore/app/AppPreferencesData.kt
@@ -0,0 +1,5 @@
+package com.dogeby.tagplayer.datastore.app
+
+data class AppPreferencesData(
+    val appThemeMode: AppThemeMode,
+)
---
+++ app/src/main/java/com/dogeby/tagplayer/datastore/app/AppThemeMode.kt
@@ -0,0 +1,5 @@
+package com.dogeby.tagplayer.datastore.app
+
+enum class AppThemeMode {
+    SYSTEM_SETTING, LIGHT, DARK
+}

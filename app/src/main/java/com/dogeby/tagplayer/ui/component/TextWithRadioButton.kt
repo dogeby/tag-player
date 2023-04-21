d9e66ef19e5ec5b29851766cae604a757c7e414b
dogeby <dogeby@gmail.com>
2023-04-22 01:24:20 +0900

    TextWithRadioButton radio button뿐만 아니라 텍스트도 클릭 가능하게 수정
    #185

--- app/src/main/java/com/dogeby/tagplayer/ui/component/TextWithRadioButton.kt
+++ app/src/main/java/com/dogeby/tagplayer/ui/component/TextWithRadioButton.kt
@@ -1,11 +1,14 @@
 package com.dogeby.tagplayer.ui.component
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -18,14 +21,29 @@ fun TextWithRadioButton(
     selected: () -> Boolean,
     onClick: () -> Unit,
     modifier: Modifier = Modifier,
+    isRowClickable: Boolean = false,
 ) {
+    val interactionSource = remember { MutableInteractionSource() }
+    val rowModifier = if (isRowClickable) {
+        modifier.clickable(
+            interactionSource = interactionSource,
+            indication = null,
+            onClick = onClick,
+        )
+    } else {
+        modifier
+    }
     Row(
-        modifier = modifier,
+        modifier = rowModifier,
         horizontalArrangement = Arrangement.SpaceBetween,
         verticalAlignment = Alignment.CenterVertically,
     ) {
         Text(text = text)
-        RadioButton(selected = selected(), onClick = onClick)
+        RadioButton(
+            selected = selected(),
+            onClick = onClick,
+            interactionSource = interactionSource,
+        )
     }
 }
 

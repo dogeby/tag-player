8a92f0480a68bb713462356f14502cc1c68a49ca
dogeby <dogeby@gmail.com>
2023-04-21 15:23:11 +0900

    ripple 로딩 효과 구현
    #186

---
+++ app/src/main/java/com/dogeby/tagplayer/ui/component/RippleLoadingEffect.kt
@@ -0,0 +1,27 @@
+package com.dogeby.tagplayer.ui.component
+
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.keyframes
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+
+@Composable
+fun rememberRippleLoadingEffectAlpha(): Float {
+    val infiniteTransition = rememberInfiniteTransition()
+    val alpha by infiniteTransition.animateFloat(
+        initialValue = 0f,
+        targetValue = 1f,
+        animationSpec = infiniteRepeatable(
+            animation = keyframes {
+                durationMillis = 1000
+                0.7f at 500
+                0.9f at 800
+            },
+            repeatMode = RepeatMode.Reverse
+        )
+    )
+    return alpha
+}

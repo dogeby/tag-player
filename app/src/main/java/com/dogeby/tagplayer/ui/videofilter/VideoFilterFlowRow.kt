8e096c2138d04311a964b51769ae2a49beb247d9
dogeby <dogeby@gmail.com>
2023-04-23 00:44:05 +0900

    VideoFilterFlowRow ripple loading 구현
    #189

--- app/src/main/java/com/dogeby/tagplayer/ui/videofilter/VideoFilterFlowRow.kt
+++ app/src/main/java/com/dogeby/tagplayer/ui/videofilter/VideoFilterFlowRow.kt
@@ -6,15 +6,21 @@ import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.dogeby.tagplayer.R
+import com.dogeby.tagplayer.ui.component.RippleLoadingVideoFilterChip
+import com.dogeby.tagplayer.ui.component.rememberRippleLoadingEffectAlpha
+import com.dogeby.tagplayer.ui.theme.RippleLoadingColor
 import com.dogeby.tagplayer.ui.theme.TagPlayerTheme
 
 @OptIn(ExperimentalLayoutApi::class)
@@ -51,6 +57,30 @@ fun FilterTitle(
     }
 }
 
+@Composable
+fun RippleLoadingVideoFilterFlowRow(
+    filterTitleName: String,
+    itemCount: Int,
+    modifier: Modifier = Modifier,
+    leadingIcon: @Composable (() -> Unit)? = null,
+    color: Color = RippleLoadingColor,
+    alpha: Float = rememberRippleLoadingEffectAlpha(),
+) {
+    VideoFilterFlowRow(
+        filterTitleName = filterTitleName,
+        modifier = modifier,
+        leadingIcon = leadingIcon,
+    ) {
+        repeat(itemCount) {
+            RippleLoadingVideoFilterChip(
+                modifier = Modifier.padding(end = dimensionResource(R.dimen.padding_small)),
+                color = color,
+                alpha = alpha,
+            )
+        }
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun FilterTitlePreview() {
@@ -63,6 +93,17 @@ fun FilterTitlePreview() {
                     contentDescription = null,
                 )
             },
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun RippleLoadingVideoFilterFlowRowPreview() {
+    TagPlayerTheme {
+        RippleLoadingVideoFilterFlowRow(
+            filterTitleName = "title",
+            itemCount = 7,
         )
     }
 }
